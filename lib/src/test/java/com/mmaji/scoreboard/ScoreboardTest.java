792570ca303d9ebf224e3133540e6d8507e21bfd
MMajidi1988 <m.majidi1367@gmail.com>
2026-03-23 21:46:24 +0100

    test: add updateScore behavior

--- lib/src/test/java/com/mmaji/scoreboard/ScoreboardTest.java
+++ lib/src/test/java/com/mmaji/scoreboard/ScoreboardTest.java
@@ -12,12 +12,25 @@ class ScoreboardTest {
         scoreboard.startMatch("Mexico", "Canada");
 
         assertEquals(1, scoreboard.getSummary().size());
-        
+
         MatchSummary match = scoreboard.getSummary().getFirst();
         assertEquals("Mexico", match.homeTeam());
         assertEquals("Canada", match.awayTeam());
         assertEquals(0, match.homeScore());
         assertEquals(0, match.awayScore());
     }
+
+    @Test
+    void updateScore_changesExistingMatchScore() {
+    Scoreboard scoreboard = new Scoreboard();
+    
+    scoreboard.startMatch("Mexico", "Canada");
+
+    scoreboard.updateScore("Mexico", "Canada", 1, 3);
+
+    MatchSummary match = scoreboard.getSummary().getFirst();
+    assertEquals(1, match.homeScore());
+    assertEquals(3, match.awayScore());
+}
     
 }
